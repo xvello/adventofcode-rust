4f7092a58d2aafddbaeb5d5ccdcf30136bb84eda
Xavier Vello <xavier.vello@gmail.com>
2020-12-08 13:07:53 +0100

    y2020d07 part 2

--- src/y2020/d07.rs
+++ src/y2020/d07.rs
@@ -1,49 +1,87 @@
-use crate::utils::{Input, CaptureParser};
+use crate::utils::{CaptureParser, Input};
 use anyhow::{bail, Result};
 use lazy_static::lazy_static;
-use regex::{Regex};
+use petgraph::graph::{DiGraph, NodeIndex};
+use petgraph::visit::EdgeRef;
+use petgraph::Direction::{Incoming, Outgoing};
+use regex::Regex;
 use std::collections::{HashMap, HashSet};
 
 lazy_static! {
     /// Regexp matching part of a rule, group 2 holds the color of the bag
-    static ref RULE_RE: regex::Regex = Regex::new(r"(\d+\s|^)(\w+ \w+) bag").unwrap();
+    static ref RULE_RE: regex::Regex = Regex::new(r"((\d+)\s|^)(\w+ \w+) bag").unwrap();
 }
 pub fn run(mut input: Input) -> Result<(usize, usize)> {
-
-    // Key is what is contained, value is set of what can contain it
-    let mut rules: HashMap<String, HashSet<String>> = HashMap::new();
+    let mut rules: Rules = Default::default();
     while let Some(Ok(line)) = input.next() {
         let mut matches = RULE_RE.captures_iter(&line);
-        let what_contains: String = match matches.next() {
+        let container = match matches.next() {
             None => bail!("Invalid input {}", line),
-            Some(m) => m.parse(2)?,
+            Some(m) => rules.get_node(m.try_get(3)?),
         };
         for m in matches {
-            let is_contained: String = m.parse(2)?;
-            match rules.get_mut(&is_contained) {
-                Some(set) =>{ set.insert(what_contains.clone());}
-                None => {
-                    let mut set = HashSet::new();
-                    set.insert(what_contains.clone());
-                    rules.insert(is_contained, set);
-                }
+            let contained = rules.get_node(m.try_get(3)?);
+            rules.add_rule(container, contained, m.parse(2)?);
+        }
+    }
+
+    let golden = rules.get_node("shiny gold");
+    Ok((
+        rules.what_can_contain(golden).len(),
+        rules.count_nested_bags(golden),
+    ))
+}
+
+#[derive(Default)]
+struct Rules {
+    pub colors: HashMap<String, NodeIndex<u32>>,
+    pub rules: DiGraph<(), u8, u32>,
+}
+
+impl Rules {
+    fn get_node(&mut self, name: &str) -> NodeIndex<u32> {
+        match self.colors.get(name) {
+            Some(idx) => *idx,
+            None => {
+                let idx = self.rules.add_node(());
+                self.colors.insert(name.to_string(), idx);
+                idx
             }
         }
     }
 
-    // Part 1: how many colors can hold a "shiny gold"
-    let mut can_contain_gold = HashSet::new();
-    let mut to_lookup = vec!["shiny gold"];
-    while let Some(color) = to_lookup.pop() {
-        if let Some(containers) = rules.get(color) {
-            for container in containers {
-                if can_contain_gold.insert(container) {
-                    // Lookup possible containers for this container if not already done
-                    to_lookup.push(container);
+    fn add_rule(&mut self, container: NodeIndex<u32>, contained: NodeIndex<u32>, count: u8) {
+        self.rules.add_edge(container, contained, count);
+    }
+
+    /// Part 1 of the problem: what colors can I find a golden bag in? Recurse up the graph
+    /// while de-duplicating colors to avoid an infinite loop
+    fn what_can_contain(&self, contained: NodeIndex<u32>) -> HashSet<NodeIndex<u32>> {
+        let mut can_contain = HashSet::new();
+        let mut to_lookup = vec![contained];
+        while let Some(color) = to_lookup.pop() {
+            for container in self.rules.neighbors_directed(color, Incoming) {
+                if can_contain.insert(container) {
+                    // We haven't seen this color yet, lookup what can contain it
+                    to_lookup.push(container)
                 }
             }
         }
+        can_contain
     }
 
-    Ok((can_contain_gold.len(), 0))
+    /// Part 2 of the problem: given a bag of a color, recurse down the graph to compute
+    /// how many total bags it would hold, the parent bag excluded
+    fn count_nested_bags(&self, container: NodeIndex<u32>) -> usize {
+        let mut total_count = 0;
+        // Iterate through all bag colors we might contain
+        for contained in self.rules.edges_directed(container, Outgoing) {
+            let count = *contained.weight() as usize;
+            // Count the bag itself
+            total_count += count;
+            // Recurse through the bags it might contain
+            total_count += count * self.count_nested_bags(contained.target());
+        }
+        total_count
+    }
 }
